5c693d779ff003d3c0934ef6dff0e7e3024cd258
mozart-rue <mozartrue@outlook.com>
2024-03-29 17:32:19 -0300

    :wrench: feat: Set theme colors

---
+++ lib/core/ui/consts/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color grey900 = Color(0xff121214);
+  static const Color grey800 = Color(0xff1C1C1F);
+  static const Color grey500 = Color(0xff50505C);
+  static const Color grey200 = Color(0xffC1C1C4);
+
+  static const Color white = Color(0xffF2F2F2);
+  static const Color black = Color(0xff000000);
+
+  static const Color green100 = Color(0xff2FD036);
+  static const Color green200 = Color(0xff23BD29);
+  static const Color green300 = Color(0xff18AB1B);
+  static const Color green500 = Color(0xff0C980E);
+  static const Color green800 = Color(0xff008500);
+}
