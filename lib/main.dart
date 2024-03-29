3a437e327ca1d7f850636769cefac34b5d5e7cec
mozart-rue <mozartrue@outlook.com>
2024-03-29 17:16:37 -0300

    :truck: refactor: Move MyApp() to app file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:game_score/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Game Score',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Container(),
-    );
-  }
 }
 
